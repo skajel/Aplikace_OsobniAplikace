bd045f3aba6087bed2eb174aede7807a7adc73c7
Martin Bures <burm10@vse.cz>
2020-12-30 15:24:08 +0100

    úprava Wallet

--- src/main/java/cz/vse/aplikace/model/Category.java
+++ src/main/java/cz/vse/aplikace/model/Category.java
@@ -1,5 +1,6 @@
 package cz.vse.aplikace.model;
 
 enum Category {
+
     RENT, ENTERTAINMENT, BUSINESS, ENERGIES, SPORT, GROCERIES, WAGE, CHILDREN
 }
--- src/main/java/cz/vse/aplikace/model/Wallet.java
+++ src/main/java/cz/vse/aplikace/model/Wallet.java
@@ -1,4 +1,45 @@
 package cz.vse.aplikace.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Wallet {
+
+    private final Map<String, Wallet> walletName;
+    public int accountBalance;
+    public String walletDesription;
+
+
+    public Wallet(String walletDesription, int accountBalance) {
+        this.accountBalance = accountBalance;
+        this.walletDesription = walletDesription;
+        walletName = new HashMap<>();
+    }
+
+
+    public void addWallet(Wallet wallet){
+
+    }
+    public void showWallet(){
+
+    }
+    public void changeWallet(){
+
+    }
+    public void deleteWallet(){
+
+    }
+
+
+    public Map<String, Wallet> getWalletName() {
+        return walletName;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public String getWalletDesription(){
+        return walletDesription;
+    }
 }
