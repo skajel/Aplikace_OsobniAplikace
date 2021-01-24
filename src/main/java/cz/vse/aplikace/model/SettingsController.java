10e29ca5908aefa5164ce55b126dd3a145c6ab16
Martin Bures <burm10@vse.cz>
2021-01-24 15:39:05 +0100

    updated SettingsScreen 2

--- src/main/java/cz/vse/aplikace/model/SettingsController.java
+++ src/main/java/cz/vse/aplikace/model/SettingsController.java
@@ -1,9 +1,11 @@
 package cz.vse.aplikace.model;
 
 
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,12 +14,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class SettingsController {
+public class SettingsController implements Initializable {
     public Button account;
     public Button add;
     public Button transaction;
@@ -31,31 +34,67 @@ public class SettingsController {
     public Button changeTheme;
     public Button logOut;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 
+    @FXML
     public void loadOverview(ActionEvent event) throws IOException {
-        BorderPane OverviewScreen = FXMLLoader.load(getClass().getResource("OverviewScreen.fxml"));
-        overview.getChildrenUnmodifiable().setAll(OverviewScreen);
+
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("OverviewScreen.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        Parent OverviewScreen = loader.load(stream);
+
+        Scene OverviewScreenScene = new Scene(OverviewScreen);
+        Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(OverviewScreenScene);
     }
 
+    @FXML
     public void loadAccount(ActionEvent event) throws IOException {
-        BorderPane AccountScreen = FXMLLoader.load(getClass().getResource("SettingsScreen.fxml"));
-        account.getChildrenUnmodifiable().setAll(AccountScreen);
+
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("SettingsScreen.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        Parent SettingsScreen = loader.load(stream);
+
+        Scene SettingScreenScene = new Scene(SettingsScreen);
+        Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(SettingScreenScene);
     }
 
+    @FXML
     public void loadAdd(ActionEvent event) throws IOException {
-        BorderPane AddScreen = FXMLLoader.load(getClass().getResource("AddScreen.fxml"));
-        add.getChildrenUnmodifiable().setAll(AddScreen);
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("AddScreen.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        Parent AddScreen = loader.load(stream);
+
+        Scene AddScreenScene = new Scene(AddScreen);
+        Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(AddScreenScene);
     }
 
+    @FXML
     public void loadTransaction(ActionEvent event) throws IOException {
-        BorderPane TransactionScreen = FXMLLoader.load(getClass().getResource("TransactionScreen.fxml"));
-        transaction.getChildrenUnmodifiable().setAll(TransactionScreen);
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("TransactionScreen.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        Parent TransactionScreen = loader.load(stream);
+
+        Scene TransactionScreenScene = new Scene(TransactionScreen);
+        Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(TransactionScreenScene);
     }
 
+    @FXML
     public void loadLogIn(ActionEvent event) throws IOException {
-        BorderPane LogInScreen = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-        logOut.getChildrenUnmodifiable().setAll(LogInScreen);
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("LoginScreen.fxml");
+        FXMLLoader loader = new FXMLLoader();
+        Parent LoginScreen = loader.load(stream);
+
+        Scene LoginScreenScene = new Scene(LoginScreen);
+        Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(LoginScreenScene);
     }
+
 }
 
 
