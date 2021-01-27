13c92e4a44b49473813f56abeeb4ebfe5db1a0e0
Ondřej Štěpán <steo08@vse.cz>
2021-01-27 21:20:01 +0100

    addController working

--- src/main/java/cz/vse/aplikace/main/Start.java
+++ src/main/java/cz/vse/aplikace/main/Start.java
@@ -1,13 +1,19 @@
 package cz.vse.aplikace.main;
 
 import cz.vse.aplikace.MainController;
+import cz.vse.aplikace.model.JSON;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.json.simple.JSONArray;
 
 import java.io.InputStream;
 
 public class Start extends Application {
     public static void main(String[] args) {
+//        if(JSON.loadData().){
+//            JSONArray array = new JSONArray();
+//            JSON.saveData(array);
+//        }
             launch();
 
     }
