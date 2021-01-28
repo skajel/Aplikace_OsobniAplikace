2fbfe0c92d94395f43d6f9e968ef86da9acc541c
Ondřej Šesták <seso00@vse.cz>
2021-01-28 22:45:26 +0100

    .

--- src/main/java/cz/vse/aplikace/model/AddController.java
+++ src/main/java/cz/vse/aplikace/model/AddController.java
@@ -222,7 +222,6 @@ public class AddController {
         addTransactionSum.clear();
         addTransactionDescription.clear();
         addTransactionDate.setValue(null);
-        add_gainComboBox.setValue(null);
         CategoryComboBox_GAIN.setValue(null);
         CategoryComboBox_SPENDING.setValue(null);
     }
