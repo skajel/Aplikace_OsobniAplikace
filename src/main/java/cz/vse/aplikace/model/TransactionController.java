59e376af4553d7e6fb10f2b72413fceb8e5b6afb
Ondřej Šesták <seso00@vse.cz>
2021-01-29 01:34:27 +0100

    6546

--- src/main/java/cz/vse/aplikace/model/TransactionController.java
+++ src/main/java/cz/vse/aplikace/model/TransactionController.java
@@ -17,7 +17,7 @@ import org.json.simple.JSONObject;
 /**
  * Třída cz.vse.aplikace.model.TransactionController je součástí aplikace pro vedení přehledu výdajů a příjmů.
  * <p>
- *
+ * <p>
  * Tato třída slouží jako controller pro TransactionScreen.fxml - která slouží k vyhledávání a filtrování transakcní podle jednotlivých kritérií
  *
  * @author Martin Bureš, Ondra Šesták, Ondra Štěpán, Lukáš Fiala, Jan Andrášek
@@ -31,124 +31,118 @@ public class TransactionController {
     public Button transaction_add;
     public Button transaction_transaction;
     public Button transaction_overview;
-    public TableView<JSONArray> tableView;
-    public FXCollections table;
+    public static TableView<JSONArray> transakcepl;
+    public static JSONArray oneTransaction;
+    public static String amount;
+    public static String category;
+    public static String date;
+    public static String description;
 
-    private final SimpleStringProperty table_amount = new SimpleStringProperty("45");
-    private final SimpleStringProperty table_category = new SimpleStringProperty("654");
-    private final SimpleStringProperty table_date = new SimpleStringProperty("546");
-    private final SimpleStringProperty table_description = new SimpleStringProperty("654");
-
-    public TransactionController() {
-        this("", "", "","");
-
-    }
-
-    public TransactionController(String table_amount, String table_category, String table_date,String table_description) {
-        setTable_amount(table_amount);
-        setTable_category(table_category);
-        setTable_date(table_date);
-        setTable_description(table_description);
-    }
+//    private final SimpleStringProperty table_amount = new SimpleStringProperty("45");
+//    private final SimpleStringProperty table_category = new SimpleStringProperty("654");
+//    private final SimpleStringProperty table_date = new SimpleStringProperty("546");
+//    private final SimpleStringProperty table_description = new SimpleStringProperty("654");
+//
+//    public TransactionController() {
+//        this("", "", "","");
+//
+//    }
+//
+//    public TransactionController(String table_amount, String table_category, String table_date,String table_description) {
+//        setTable_amount(table_amount);
+//        setTable_category(table_category);
+//        setTable_date(table_date);
+//        setTable_description(table_description);
+//    }
 
     public static void initialize() {
-        getTransakce();
-        TableColumn<JSONArray,String> amountCollumn = new TableColumn<>("Amount");
-        amountCollumn.setPrefWidth(100);
-        amountCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.AMOUNT));
 
-        TableColumn<JSONArray,String> categoryCollumn = new TableColumn<>("Category");
-        categoryCollumn.setPrefWidth(100);
-        categoryCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.CATEGORY));
-
-        TableColumn<JSONArray,String> dateCollumn = new TableColumn<>("Date");
-        dateCollumn.setPrefWidth(100);
-        dateCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.DATE));
-
-        TableColumn<JSONArray,String> descriptionCollumn = new TableColumn<>("Description");
-        descriptionCollumn.setPrefWidth(100);
-        descriptionCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.DESCRIPTION));
+//        iterateTransactions(getTransaction());
+//        TableColumn<String, String> amountCollumn = new TableColumn<>("Amount");
+//        amountCollumn.setPrefWidth(100);
+//        amountCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.AMOUNT.toString()));
+//
+//
+//        TableColumn<String, String> categoryCollumn = new TableColumn<>("Category");
+//        categoryCollumn.setPrefWidth(100);
+//        categoryCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.CATEGORY.toString()));
+//
+//        TableColumn<String, String> dateCollumn = new TableColumn<>("Date");
+//        dateCollumn.setPrefWidth(100);
+//        dateCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.DATE.toString()));
+//
+//        TableColumn<String, String> descriptionCollumn = new TableColumn<>("Description");
+//        descriptionCollumn.setPrefWidth(100);
+//        descriptionCollumn.setCellValueFactory(new PropertyValueFactory<>(MainController.DESCRIPTION.toString()));
     }
 
 
-
-
-    public static ObservableList<JSONArray> getTransakce(){
+    public static JSONArray getTransaction() {
         JSONObject user = JSON.getCurrentUser();
-        JSONArray transactions  = (JSONArray) user.get(MainController.TRANSACTIONS);
-        ObservableList<JSONArray> transakcepl = FXCollections.observableArrayList();
-        transakcepl.add(transactions);
-        return transakcepl;
-
+        JSONArray transactions = (JSONArray) user.get(MainController.TRANSACTIONS);
+        return transactions;
     }
 
-    public void update(ActionEvent actionEvent) {getTransakce();
-    initialize();
+    public static JSONObject iterateTransactions(JSONArray transactions) {
+        transactions.forEach(transaction -> {
+            oneTransaction = (JSONObject) ((JSONObject) transaction).get(MainController.TRANSACTIONS);
+        });}
+
+//            oneTransaction.forEach(transaction2 -> {
+//                 amount = (String) ((JSONObject) transaction2).get(MainController.AMOUNT);
+//                 categoty = (String) ((JSONObject) transaction2).get(MainController.CATEGORY);
+//                 date = (String) ((JSONObject) transaction2).get(MainController.DATE);
+//                 description = (String) ((JSONObject) transaction2).get(MainController.DESCRIPTION);
+//
+
+
+        public static void iterateTransactions2 (JSONArray oneTransaction){
+            oneTransaction.forEach(transaction -> {
+                amount = (String) ((JSONObject) transaction).get(MainController.AMOUNT);
+                category = (String) ((JSONObject) transaction).get(MainController.CATEGORY);
+                date = (String) ((JSONObject) transaction).get(MainController.DATE);
+                description = (String) ((JSONObject) transaction).get(MainController.DESCRIPTION);
+            });
+        }
+
+               ObservableList < JSONArray > transakcepl = FXCollections.observableArrayList();
+                transakcepl.add(new JSONArray(amount,category));
+
+        public void update (ActionEvent actionEvent){
+            iterateTransactions(getTransaction());
+        }
+
+
+        /**
+         * Metoda, která upravuje Button - Overview, provede změnu scény na OverviewScreen.fxml
+         */
+        public void loadOverview () {
+            transaction_overview.setCursor(Cursor.HAND);
+            Menu.loadOverview();
+        }
+
+        /**
+         * Metoda, která upravuje Button - Account, provede změnu scény na SettingsScreen.fxml
+         */
+        public void loadAccount () {
+            transaction_account.setCursor(Cursor.HAND);
+            Menu.loadAccount();
+        }
+
+        /**
+         * Metoda, která upravuje Button - Add, provede změnu scény na AddScreen.fxml
+         */
+        public void loadAdd () {
+            transaction_add.setCursor(Cursor.HAND);
+            Menu.loadAdd();
+        }
+
+        /**
+         * Metoda, která upravuje Button - Transaction, provede změnu scény na TransactionScreen.fxml
+         */
+        public void loadTransaction () {
+            transaction_transaction.setCursor(Cursor.CLOSED_HAND);
+        }
+
+
     }
-
-
-    public String getTable_amount() {
-        return table_amount.get();
-    }
-
-
-    public void setTable_amount(String table_amount) {
-        this.table_amount.set(table_amount);
-    }
-
-    public String getTable_category() {
-        return table_category.get();
-    }
-
-    public void setTable_category(String table_category) {
-        this.table_category.set(table_category);
-    }
-
-    public String getTable_date() {
-        return table_date.get();
-    }
-
-
-    public void setTable_date(String table_date) {
-        this.table_date.set(table_date);
-    }
-
-    public String getTable_description() {
-        return table_description.get();
-    }
-
-    public void setTable_description(String table_description) {
-        this.table_description.set(table_description);
-    }
-
-
-
-
-
-
-    /**
-     * Metoda, která upravuje Button - Overview, provede změnu scény na OverviewScreen.fxml
-     */
-    public void loadOverview() {
-        transaction_overview.setCursor(Cursor.HAND);
-        Menu.loadOverview();}
-    /**
-     * Metoda, která upravuje Button - Account, provede změnu scény na SettingsScreen.fxml
-     */
-    public void loadAccount(){
-        transaction_account.setCursor(Cursor.HAND);
-        Menu.loadAccount();}
-    /**
-     * Metoda, která upravuje Button - Add, provede změnu scény na AddScreen.fxml
-     */
-    public void loadAdd(){
-        transaction_add.setCursor(Cursor.HAND);
-        Menu.loadAdd(); }
-    /**
-     * Metoda, která upravuje Button - Transaction, provede změnu scény na TransactionScreen.fxml
-     */
-    public void loadTransaction(){
-        transaction_transaction.setCursor(Cursor.CLOSED_HAND);}
-
-
-}
